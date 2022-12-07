dbc0cd5367322a3ab1d94b72a87a078c497e316e
Alexander Carlström <caarlstrom.alexander@gmail.com>
2022-12-07 06:00:21 +0100

    Completed day 6

---
+++ day6/part1.js
@@ -0,0 +1,22 @@
+import { getAllLines, getNumbersFromString } from '../global.js';
+import _ from 'lodash';
+
+const input = getAllLines('day6/input.txt');
+input.forEach((line) => {
+  let letters = [];
+
+  loop: for (let i = 0; i < line.length; i++) {
+    if (_.includes(letters, line[i])) {
+      const index = _.indexOf(letters, line[i]);
+      letters = letters.slice(index + 1);
+      letters.push(line[i]);
+    } else {
+      letters.push(line[i]);
+
+      if (letters.length === 4) {
+        console.log(i + 1);
+        break loop;
+      }
+    }
+  }
+});
