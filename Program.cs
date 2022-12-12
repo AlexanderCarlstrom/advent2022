4b7691aca5ea9a08c457d7762cfb5e9fdfe3eb1e
Alexander Carlström <alexander.carlstrom@tietoevry.com>
2022-12-12 13:16:15 +0100

    Completed day 12

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Advent2022.Day12;
+
+Console.WriteLine("Hello, World!");
+var day12 = new Day12(@"C:\DeveloperArea\advent2022\Advent2022\Day12\input.txt");
+day12.Part2();
---
+++ Utils.cs
@@ -0,0 +1,10 @@
+﻿namespace Advent2022;
+
+public static class Utils
+{
+    public static int GetAlphabeticalPosition(char c)
+    {
+        var alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray().ToList();
+        return alphabet.IndexOf(c);
+    }
+}
